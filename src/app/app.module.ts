f7180300b4126ed686c82b2599491f940c971312
Subhransu <subhransu@Subhransus-Mini.lan>
2024-12-30 00:44:13 -0600

    4th commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,53 +6,34 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { LoginComponent } from './pages/login/login.component';
-// Uncomment and import additional components or modules as needed
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
-// import { PostsComponent } from './pages/posts/posts.component';
-   import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-   import { MatFormFieldModule } from '@angular/material/form-field';
-   import { MatInputModule } from '@angular/material/input';
-   import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatMenuModule } from '@angular/material/menu';
-// import { MatCardModule } from '@angular/material/card';
-// import { AngularFireModule } from '@angular/fire';
-// import { AngularFireStorageModule } from '@angular/fire/storage';
-// import { environment } from 'src/environments/environment';
-// import { MaterialFileInputModule } from 'ngx-material-file-input';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent, 
+    TopbarComponent,
     LoginComponent,
-    // Add additional components as needed
     CreateAccountComponent,
-    // PostsComponent,
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
-    // Add additional modules as needed
-       BrowserAnimationsModule,
-       MatFormFieldModule,
-       MatInputModule,
-       ReactiveFormsModule,
-       FormsModule,
-    // HttpClientModule,
-    // MatSnackBarModule,
-    // MatIconModule,
-    // MatMenuModule,
-    // MatCardModule,
-    // AngularFireModule.initializeApp(environment.firebaseConfig),
-    // MaterialFileInputModule,
-    // AngularFireStorageModule,
-    RouterModule.forRoot([]), // Define routes here or import AppRoutingModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot([]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()), // Replaces deprecated HttpClientModule
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
-
--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -4,32 +4,44 @@ import { MatInputModule } from '@angular/material/input';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-create-account',
+  standalone: true,
   imports: [
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './create-account.component.html',
-  styleUrl: './create-account.component.css'
+  styleUrls: ['./create-account.component.css'],
 })
-export class CreateAccountComponent implements OnInit{
-   
-  createAccountForm! : FormGroup; 
+export class CreateAccountComponent implements OnInit {
+  createAccountForm!: FormGroup;
 
-  constructor(private fb:FormBuilder) {}
-  
+  constructor(private fb: FormBuilder, public userService: UserService) {}
+
   ngOnInit(): void {
     this.createAccountForm = this.fb.group({
-      email:['', [Validators.required, Validators.email]],
-      username: ['', [Validators.required, Validators.minLength(10)]], 
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
+  create(): void {
+    this.userService
+      .createNewUser(this.createAccountForm.value)
+      .then((res) => {
+        console.log(res);
+        // Navigate or store user data as needed
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
